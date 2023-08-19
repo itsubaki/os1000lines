53f697b2b465fc7d4a714e1b18ab28731790adb3
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-19 23:48:12 +0900

    Revert panic test

--- kernel.c
+++ kernel.c
@@ -29,17 +29,15 @@ void putchar(char ch)
 
 void kernel_main(void)
 {
-
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
-    PANIC("booted!");
-    printf("unreachable here!\n");
-    // printf("\n\nHello %s\n", "World!");
-    // printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
-    // for (;;)
-    // {
-    //     __asm__ __volatile__("wfi");
-    // }
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+
+    for (;;)
+    {
+        __asm__ __volatile__("wfi");
+    }
 }
 
 __attribute__((section(".text.boot")))
