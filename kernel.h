af268274e9f8e431f955e5da22f312d7d52faa66
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-20 13:23:15 +0900

    Add process

--- kernel.h
+++ kernel.h
@@ -66,3 +66,15 @@ struct sbiret
         {                                                                     \
         }                                                                     \
     } while (0)
+
+#define PROCS_MAX 8
+#define PROC_UNUSED 0
+#define PROC_RUNNABLE 1
+
+struct process
+{
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
