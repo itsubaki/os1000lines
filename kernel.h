0a9ff787f94664104f1ee0df17a36e00449f783e
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-08-27 12:18:46 +0900

    Add system call

--- kernel.h
+++ kernel.h
@@ -13,6 +13,8 @@
 #define PAGE_U (1 << 4)
 #define USER_BASE 0x1000000
 #define SSTATUS_SPIE (1 << 5)
+#define SCAUSE_ECALL 8
+#define PROC_EXITED 2
 
 #define READ_CSR(reg)                         \
     ({                                        \
